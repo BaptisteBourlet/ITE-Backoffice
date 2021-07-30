7a7d4a9a41849cf56d193856e348d33d901f4e1a
unknown <lap.hoang.idi@gmail.com>
2021-07-30 10:54:16 +0200

    edit finished

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -56,6 +56,13 @@ router.post('/searchSerie', apiCtrl.searchSerie);
 
 router.post('/addSeries', apiCtrl.addSeries);
 
+router.post('/editSeries', apiCtrl.editSeries);
+
+router.post('/getOtherLanguageDetailSerie', apiCtrl.getOtherLanguageDetailSerie);
+
+
+
+
 
 
 
