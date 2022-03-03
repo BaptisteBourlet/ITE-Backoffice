e104c90ff16fcf95f8a198508c53a8bd4634488f
BaptisteBourlet <baptiste.bourlet@gmail.com>
2022-03-03 15:36:26 +0100

    Labels translations

--- backoffice/controller/labels-ctrl.js
+++ backoffice/controller/labels-ctrl.js
@@ -14,7 +14,7 @@ const con = mysql.createConnection({
    multipleStatements: true,
 })
 
-const LANGUAGES = ['en', 'de', 'es', 'fr', 'ru'];
+const LANGUAGES = ['en', 'de', 'sp', 'fr', 'ru'];
 
 exports.getLabels = async (req, res) => {
     let query = 'SELECT * FROM Labels order by Labels.Key';
@@ -154,49 +154,139 @@ exports.getLabels = async (req, res) => {
        RULanguage, RUValue
     } = req.body;
  
+    //INSERT QUERY
+    const IENQuery = `INSERT INTO Labels (Labels.Key, Language, Labels.Value) VALUES ("${Key}", "${FRLanguage}", "${FRValue}");`
+    
+    const IFRQuery = `INSERT INTO Labels (Labels.Key, Language, Labels.Value) VALUES ("${Key}", "${FRLanguage}", "${FRValue}");`
+    const IDEQuery = `INSERT INTO Labels (Labels.Key, Language, Labels.Value) VALUES ("${Key}", "${DELanguage}", "${DEValue}");`
+    const ISPQuery = `INSERT INTO Labels (Labels.Key, Language, Labels.Value) VALUES ("${Key}", "${SPLanguage}", "${SPValue}");`
+    const IRUQuery = `INSERT INTO Labels (Labels.Key, Language, Labels.Value) VALUES ("${Key}", "${RULanguage}", "${RUValue}");`
+    
+    //check if datas for translations exists
+    const existTransEN = `SELECT COUNT(*) AS transCount FROM Labels WHERE Labels.Key = "${oldKey}" AND Language = 'en';`;
+    const existTransFR = `SELECT COUNT(*) AS transCountFR FROM Labels WHERE Labels.Key = "${oldKey}" AND Language = 'fr';`;
+    const existTransDE = `SELECT COUNT(*) AS transCountDE FROM Labels WHERE Labels.Key = "${oldKey}" AND Language = 'de';`;
+    const existTransES = `SELECT COUNT(*) AS transCountES FROM Labels WHERE Labels.Key = "${oldKey}" AND Language = 'sp';`;
+    const existTransRU = `SELECT COUNT(*) AS transCountRU FROM Labels WHERE Labels.Key = "${oldKey}" AND Language = 'ru';`;
  
     if (Value !== "") {
-       con.query(`UPDATE Labels SET Labels.Key = "${Key}", Language = "${Language}", Labels.Value = "${Value}" WHERE Language = 'en' AND Labels.Key = "${oldKey}";`, (err, results, fields) => {
-          if (err) throw err;
- 
-          res.status(200).send(results);
-       });
+      con.query(existTransEN, (err, transResult) => {
+         if (err) throw err;
+
+         if (transResult[0].transCount > 0) {                 // 2 - if TRUE =>  update Translations
+            con.query(`UPDATE Labels SET Labels.Key = "${Key}", Language = "${Language}", Labels.Value = "${Value}" WHERE Language = 'en' AND Labels.Key = "${oldKey}";`, (err, results, fields) => {
+               if (err) throw err;
+      
+               res.status(200).send(results);
+            });
+         }
+         else {                                             // 2 - if FALSE => insert NEW record
+            con.query(IENQuery, (err, insertResult) => {
+               if (err) {
+                  console.log('INSERT ERRRROR', err);
+               }
+               console.log('insert Serie english translations')
+            })
+         }
+      })
+
+       
     } else {
        console.log('english wasnt filled in')
     }
  
     if (FRValue !== '') {
-       con.query(`UPDATE Labels SET Labels.Key = "${Key}", Language = "${FRLanguage}", Labels.Value = "${FRValue}" WHERE Language = 'fr' AND Labels.Key = "${oldKey}";`, (err, results, fields) => {
-          if (err) throw err;
- 
-       });
+      con.query(existTransFR, (err, transResult) => {
+         if (err) throw err;
+
+         if (transResult[0].transCountFR > 0) {                 // 2 - if TRUE =>  update Translations
+            con.query(`UPDATE Labels SET Labels.Key = "${Key}", Language = "${FRLanguage}", Labels.Value = "${FRValue}" WHERE Language = 'fr' AND Labels.Key = "${oldKey}";`, (err, results, fields) => {
+               if (err) throw err;
+      
+            });
+         }
+         else {                                             // 2 - if FALSE => insert NEW record
+            con.query(IFRQuery, (err, insertResult) => {
+               if (err) {
+                  console.log('INSERT ERRRROR', err);
+               }
+               console.log('insert Serie French translations')
+            })
+         }
+      })
+       
     } else {
        console.log('french wasnt filled in')
     }
  
     if (DEValue !== '') {
-       con.query(`UPDATE Labels SET Labels.Key = "${Key}", Language = "${DELanguage}", Labels.Value = "${DEValue}" WHERE Language = 'de' AND Labels.Key = "${oldKey}";`, (err, results, fields) => {
-          if (err) throw err;
- 
-       });
+      con.query(existTransDE, (err, transResult) => {
+         if (err) throw err;
+
+         if (transResult[0].transCountDE > 0) {                 // 2 - if TRUE =>  update Translations
+            con.query(`UPDATE Labels SET Labels.Key = "${Key}", Language = "${DELanguage}", Labels.Value = "${DEValue}" WHERE Language = 'de' AND Labels.Key = "${oldKey}";`, (err, results, fields) => {
+               if (err) throw err;
+      
+            });
+         }
+         else {                                             // 2 - if FALSE => insert NEW record
+            con.query(IDEQuery, (err, insertResult) => {
+               if (err) {
+                  console.log('INSERT ERRRROR', err);
+               }
+               console.log('insert Serie DE translations')
+            })
+         }
+      })
+       
     } else {
        console.log('german wasnt filled in')
     }
  
     if (RUValue !== '') {
-       con.query(`UPDATE Labels SET Labels.Key = "${Key}", Language = "${RULanguage}", Labels.Value = "${RUValue}" WHERE Language = 'ru' AND Labels.Key = "${oldKey}";`, (err, results, fields) => {
-          if (err) throw err;
- 
-       });
+      con.query(existTransRU, (err, transResult) => {
+         if (err) throw err;
+
+         if (transResult[0].transCountRU > 0) {                 // 2 - if TRUE =>  update Translations
+            con.query(`UPDATE Labels SET Labels.Key = "${Key}", Language = "${RULanguage}", Labels.Value = "${RUValue}" WHERE Language = 'ru' AND Labels.Key = "${oldKey}";`, (err, results, fields) => {
+               if (err) throw err;
+      
+            });
+         }
+         else {                                             // 2 - if FALSE => insert NEW record
+            con.query(IRUQuery, (err, insertResult) => {
+               if (err) {
+                  console.log('INSERT ERRRROR', err);
+               }
+               console.log('insert Serie RU translations')
+            })
+         }
+      })
+       
     } else {
        console.log('russian wasnt filled in')
     }
  
     if (SPValue !== '') {
-       con.query(`UPDATE Labels SET Labels.Key = "${Key}", Language = "${SPLanguage}", Labels.Value = "${SPValue}" WHERE Language = 'es' AND Labels.Key = "${oldKey}";`, (err, results, fields) => {
-          if (err) throw err;
- 
-       });
+      con.query(existTransES, (err, transResult) => {
+         if (err) throw err;
+
+         if (transResult[0].transCountES > 0) {                 // 2 - if TRUE =>  update Translations
+            con.query(`UPDATE Labels SET Labels.Key = "${Key}", Language = "${SPLanguage}", Labels.Value = "${SPValue}" WHERE Language = 'sp' AND Labels.Key = "${oldKey}";`, (err, results, fields) => {
+               if (err) throw err;
+      
+            });
+         }
+         else {                                             // 2 - if FALSE => insert NEW record
+            con.query(ISPQuery, (err, insertResult) => {
+               if (err) {
+                  console.log('INSERT ERRRROR', err);
+               }
+               console.log('insert Serie ES translations')
+            })
+         }
+      })
+       
     } else {
        console.log('spanish wasnt filled in')
     }
